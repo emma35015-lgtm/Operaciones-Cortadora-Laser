b164bbe37e4fed8a59445bcbda1c952fdc4ead05
emma35015-lgtm <emma35015-lgtm@users.noreply.github.com>
2026-06-06 21:15:00 +0000

    deploy: d35acdc607e3d9b9a349d212752907a27a160bfa

--- _next/static/chunks/0~ilotscdsj17.js
+++ _next/static/chunks/0~ilotscdsj17.js
@@ -59,7 +59,7 @@ Read more: https://nextjs.org/docs/messages/next-image-unconfigured-localpattern
         }
         .herr-badge {
           position: absolute;
-          top: clamp(22px, 4vh, 42px);
+          top: clamp(68px, 9vh, 88px);
           left: 50%;
           transform: translateX(-50%);
           z-index: 5;
@@ -102,7 +102,7 @@ Read more: https://nextjs.org/docs/messages/next-image-unconfigured-localpattern
           animation: herrRise 1s cubic-bezier(.2,.7,.2,1) both;
         }
         .herr-logo {
-          width: clamp(260px, 44vw, 620px);
+          width: clamp(320px, 56vw, 780px);
           height: auto;
           display: block;
         }
